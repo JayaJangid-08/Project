ffb0f9912d2b7e342bbfd67309413ac70690a4be
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-14 20:39:39 +0530

    Serializer Added

---
+++ TaskManagement/Tasks/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Task , Project
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        # fields = '__all__'
+        fields = ['id','title','description','project','assigned_to','created_by','status','priority','due_date','created_at','updated_at']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        # fields = '__all__'
+        fields = ['id','name','description','members','created_by','status','start_date','end_date','created_at','updated_at']
+        read_only_fields = ['created_by' , 'created_at' , 'id']  # id bhi read only hona chahiye

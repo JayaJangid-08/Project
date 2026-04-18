12b4462218f6745a3a41cfc3a4ea8488e8078743
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-18 12:52:50 +0530

    Updated Settings

--- TaskManagement/TaskManagement/settings.py
+++ TaskManagement/TaskManagement/settings.py
@@ -105,6 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+}
+
+REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],

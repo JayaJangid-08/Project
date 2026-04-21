2d3ea0c08d4d21dd3aa42c7ee71841f21217db7b
JayaJangid-08 <jayajangiddhamu05@gmail.com>
2026-04-21 10:33:12 +0530

    EMail Format

--- TaskManagement/Tasks/email.py
+++ TaskManagement/Tasks/email.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.core.mail import EmailMessage
+from celery import shared_task
 
 
+@shared_task
 def send_task_assignment_email(assigned_to, task, assigned_by):
     subject = "New Task Assigned"
     message = f"You have been assigned a new task: {task.title} in project {task.project.name} by {assigned_by.username}"
@@ -10,8 +12,15 @@ def send_task_assignment_email(assigned_to, task, assigned_by):
             body=message,
             from_email=settings.DEFAULT_FROM_EMAIL,
             to=[assigned_to.email],
-            reply_to=[assigned_by.email],
-        )
-    email.send(fail_silently=False)
+            )
+    
+    try:
+        email.send(fail_silently=False)
+        print("EMAIL SENT to", assigned_to.email)
+        return True
+    
+    except Exception as e:
+        print("EMAIL FAILED", e)
+        return False
 
 
